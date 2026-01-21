5ffcee09fd4c43d53d00164ca45ee4662655b7d0
Chua Yong Liang <e0702014@u.nus.edu>
2026-01-21 17:59:17 +0800

    Level 0: Renamed Chatbot, Greet and Exit

---
+++ src/main/java/SWE.java
@@ -0,0 +1,17 @@
+public class SWE {
+    public static void main(String[] args) {
+        //gen AI was used to generate the logo
+        String logo = " ____  __        __ _____\n"
+                + "/ ___| \\ \\      / /| ____|\n"
+                + "\\___ \\  \\ \\ /\\ / / |  _|  \n"
+                + " ___) |  \\ V  V /  | |___ \n"
+                + "|____/    \\_/\\_/   |_____|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.print("____________________________________________________________\n" +
+                " Hello! I'm SWE\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
