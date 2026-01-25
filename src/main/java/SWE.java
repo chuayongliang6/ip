72c297e542d957e75f19cffcfc5b247ed454190a
Chua Yong Liang <e0702014@u.nus.edu>
2026-01-25 09:21:39 +0800

    Level-3: Add task completion status tracking
    Tasks are currently stored as simple strings, which does not allow
    for tracking whether a task is completed or pending.
    
    Users need a way to see their progress and toggle the status of
    tasks in their list.
    
    Let's,
    * create a Task class to store the description and isDone status
    * implement 'mark' command to update a task to completed
    * add status icons [X] and [ ] to the 'list' output via Task.toString()
    
    The Task class is used instead of a String array.

--- src/main/java/SWE.java
+++ src/main/java/SWE.java
@@ -13,22 +13,34 @@ public class SWE {
                 " Hello! I'm SWE\n" +
                 " What can I do for you?\uD83D\uDE00\n" +
                 "____________________________________________________________\n");
-        String[] userTask = new String[100];
+        Task[] userTask = new Task[100];
         int taskIndex = 0;
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while (!line.equals("bye")) {
             System.out.print("____________________________________________________________\n");
-            if (!line.equals("list")) {
-                userTask[taskIndex] = line;
+            // listing tasks
+            if (line.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i<taskIndex; i++) {
+                    System.out.println((i+1) + ". " + userTask[i].toString());
+                }
+            }
+            //marking tasks
+            else if (line.startsWith("mark ")) {
+                String[] markNumber = line.split(" ");
+                // minus 1 because if u mark task 2, it is at index 1
+                int markIndex = Integer.parseInt(markNumber[1]) - 1;
+                userTask[markIndex].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(userTask[markIndex].toString());
+            }
+            //adding tasks
+            else {
+                userTask[taskIndex] = new Task(line);
                 taskIndex++;
                 System.out.println("added: " + line);
-            }
-            else {
-                for (int i = 0; i<taskIndex; i++) {
-                    System.out.println((i+1) + ". " + userTask[i]);
-                }
             }
             System.out.print("____________________________________________________________\n");
             line = in.nextLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        // mark done task with X
+        return (isDone ? "X" : " ");
+    }
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
