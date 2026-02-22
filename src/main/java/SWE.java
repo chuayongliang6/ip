f486ac5c0f541c72e8c2cf93a8a34a846f28e3f2
Chua Yong Liang <e0702014@u.nus.edu>
2026-02-22 11:53:24 +0800

    Moved relevant logic from main to TaskList class

--- src/main/java/SWE.java
+++ src/main/java/SWE.java
@@ -6,13 +6,10 @@ import java.util.ArrayList;
  * It handles user input and manages a list of tasks.
  */
 public class SWE {
-
-
     private static final String LIST_COMMAND = "list";
     private static final String MARK_COMMAND = "mark ";
     private static final String BYE_COMMAND = "bye";
     private static final String DELETE_COMMAND = "delete";
-
 
     /**
      * The entry point of the application. Prints a welcome message, processes user commands, and says goodbye.
@@ -25,13 +22,10 @@ public class SWE {
         Ui.printGoodbye();
     }
 
-
     private static void processCommand() {
         ArrayList<Task> userTasks = new ArrayList<>();
         Storage.loadTasks(userTasks);
         String line = Ui.readCommand();
-
-
         while (!line.equals(BYE_COMMAND)) {
             Ui.printBorder();
             handleCommand(line, userTasks);
@@ -40,59 +34,24 @@ public class SWE {
         }
     }
 
-
     private static void handleCommand(String line, ArrayList<Task> userTasks) {
         try {
             if (line.equals(LIST_COMMAND)) {
                 Ui.listTasks(userTasks);
             } else if (line.startsWith(MARK_COMMAND)) {
-                markTasks(line, userTasks);
+                TaskList.markTasks(line, userTasks);
                 Storage.saveTasks(userTasks);
                 // Delete tasks
             } else if (line.startsWith(DELETE_COMMAND)) {
-                deleteTasks(line, userTasks);
+                TaskList.deleteTasks(line, userTasks);
                 Storage.saveTasks(userTasks);
             } else {
                 // Adding tasks
-                addTasks(userTasks, line);
+                TaskList.addTasks(userTasks, line);
                 Storage.saveTasks(userTasks);
             }
         } catch (SWEException e) {
             System.out.println(e.getMessage());
         }
     }
-
-//    private static void listTasks(ArrayList<Task> userTasks) {
-//        System.out.println("Here are the tasks in your list:");
-//        for (int i = 0; i < userTasks.size(); i++) {
-//            System.out.println((i + 1) + ". " + userTasks.get(i).toString());
-//        }
-//    }
-
-    private static void markTasks(String line, ArrayList<Task> userTasks) {
-        int markIndex = getIndex(line);
-        Task taskToMark = userTasks.get(markIndex);
-        taskToMark.markAsDone();
-        Ui.printMarkTask(taskToMark);
-    }
-
-    private static int getIndex(String line) {
-        String[] parts = line.split(" ");
-        return Integer.parseInt(parts[1]) - 1;
-    }
-
-    private static void deleteTasks(String line, ArrayList<Task> userTasks) {
-        int deleteIndex = getIndex(line);
-        //Deletes the task and assigns deleted task to the deletedTask variable to print the deleted task details
-        Task deletedTask = userTasks.remove(deleteIndex);
-        Ui.printDeleteTask(deletedTask, userTasks);
-    }
-
-    private static void addTasks(ArrayList<Task> userTasks, String line) throws SWEException {
-        Task newTask = Parser.createTask(line);
-        userTasks.add(newTask);
-        Ui.printAddTask(newTask, userTasks);
-    }
-
-
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class TaskList {
+    private static int getIndex(String line) {
+        String[] parts = line.split(" ");
+        return Integer.parseInt(parts[1]) - 1;
+    }
+
+    public static void markTasks(String line, ArrayList<Task> userTasks) {
+        int markIndex = getIndex(line);
+        Task taskToMark = userTasks.get(markIndex);
+        taskToMark.markAsDone();
+        Ui.printMarkTask(taskToMark);
+    }
+
+    public static void addTasks(ArrayList<Task> userTasks, String line) throws SWEException {
+        Task newTask = Parser.createTask(line);
+        userTasks.add(newTask);
+        Ui.printAddTask(newTask, userTasks);
+    }
+
+    public static void deleteTasks(String line, ArrayList<Task> userTasks) {
+        int deleteIndex = getIndex(line);
+        //Deletes the task and assigns deleted task to the deletedTask variable to print the deleted task details
+        Task deletedTask = userTasks.remove(deleteIndex);
+        Ui.printDeleteTask(deletedTask, userTasks);
+    }
 }
